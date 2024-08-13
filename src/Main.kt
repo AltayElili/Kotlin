d21137a9ae380023a0a8c6b17c1f49773e3f17fb
Altay Elili <altayalili28@gmail.com>
2024-08-13 22:28:30 +0400

    commit

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,67 @@
 fun main() {
-    println("Hello")
 
+//val myList1= mutableListOf(10,12,4,6,11)
+//    println(myList1.removeFirstOrNull())
+
+    val myList4= mutableListOf(3,4,5,1,7,8)
+    println(myList4.maxByOrNull {
+        it>5
+    })
+}
+
+//Find
+fun <T> myFind(myList1: MutableList<T>, condition:(T)-> Boolean):T?{
+    var result:T?=null
+    for(item in myList1){
+        if(condition(item)){
+            result=item
+            return result
+        }
+    }
+    return null
+}
+
+//FindLast
+fun <T> myFindLast(myList2:MutableList<T>, condition: (T) -> Boolean):T?{
+    val myNewList= myList2.reversed()
+    for(item in myNewList){
+        if(condition(item)){
+            return item
+        }
+    }
+    return null
+}
+//removeFirstorNull
+fun <T> myRemoveFirstOrNull(myList3: MutableList<T>):T?{
+    return if(myList3.isNotEmpty()){
+        myList3.removeAt(0)
+    }
+    else {
+        null
+    }
+}
+
+//FirstOrNull
+fun <T> myFirstOrNull(myList4: MutableList<T>):T?{
+    return if(myList4.isNotEmpty()){
+        myList4[0]
+    }
+    else {
+        null
+    }
+}
+
+//maxByOrNull
+fun <T,K> myMaxByOrNull(myList5: MutableList<T>, condition: (T) -> K):T?{
+    if(myList5.isEmpty()) return null
+    var maxElement=myList5[0]
+    var maxValue=condition(maxElement)
+    for(element in myList5){
+        val value = condition(element)
+        if(value>maxValue){
+            maxElement=element
+            maxValue=value
+        }
+    }
+    return maxElement
 }
